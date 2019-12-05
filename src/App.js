3bc09e3a35ea2cef1a7093fab6ad0ea6b4c60c02
Jon Struve <ocdammo3@gmail.com>
2019-12-05 09:31:27 -0600

    Added hand component that currently renders all cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/header';
 import DeckStack from './components/deck-stack';
+import Hand from './components/hand';
 
 function App() {
   return (
     <>
       <Header/>
       <DeckStack/>
+      <Hand/>
     </>
   );
 }
