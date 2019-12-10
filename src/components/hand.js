6d442909a7c01aca1587db0752540f0705c3fbe6
Jon Struve <ocdammo3@gmail.com>
2019-12-10 13:55:44 -0600

    Temporary posting of every player's cards to page

--- src/components/hand.js
+++ src/components/hand.js
@@ -4,18 +4,66 @@ import { fullDeckStack } from '../full-deck';
 
 export default function Hand(props) {
   let deck = new Deck(fullDeckStack);
-  console.log(deck.cards);
+  let dealtHand = deck.dealHand();
   return (
-    <div className="hand">
-      <ul>
-        {
-          deck.cards.map(card => (
-            <li>
-              <img src={card.image} alt={card.name} className={`${card.id} card-image`}/>
-            </li>
-          ))
-        } 
-      </ul>   
-    </div>
+    <>
+      <div className="hand">
+        <ul>
+          {
+            dealtHand.player1Hand.map(card => {
+              try {
+                return (
+                <li key={card.id}>
+                  <img src={card.image} alt={card.name} className={`${card.id} card-image`}/>
+                </li>
+              )
+              } catch(error) {
+                return (
+                  <h1 key="error">Error</h1>
+                )
+              }
+            })
+          } 
+        </ul> 
+      </div>
+      <div className="hand">
+        <ul>
+          {
+            dealtHand.player2Hand.map(card => {
+              try {
+                return (
+                <li key={card.id}>
+                  <img src={card.image} alt={card.name} className={`${card.id} card-image`}/>
+                </li>
+              )
+              } catch(error) {
+                return (
+                  <h1 key="error">Error</h1>
+                )
+              }
+            })
+          } 
+        </ul> 
+      </div>
+      <div className="hand">
+        <ul>
+          {
+            dealtHand.player3Hand.map(card => {
+              try {
+                return (
+                <li key={card.id}>
+                  <img src={card.image} alt={card.name} className={`${card.id} card-image`}/>
+                </li>
+              )
+              } catch(error) {
+                return (
+                  <h1 key="error">Error</h1>
+                )
+              }
+            })
+          } 
+        </ul> 
+      </div>
+    </>
   )
 }
