db0aa7e43d29d0d96947ba68dd323bd020021133
Jon Struve <ocdammo3@gmail.com>
2019-12-10 13:54:21 -0600

    Fixed getCard function to return only numbers between 0 and 51

--- src/deck-creator.js
+++ src/deck-creator.js
@@ -37,6 +37,6 @@ export default class Deck {
   }
 
   getCard = () => {
-    return this.cards[randomCard];
+    return this.cards[Math.ceil(((Math.ceil(Math.random() * 52)) + Math.ceil(Math.random() * 52)) / 2) - 1];
   }
 }
