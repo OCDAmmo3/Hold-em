6270f957a88c6a9709f1985948de86b62c6ef596
Jon Struve <ocdammo3@gmail.com>
2020-01-04 12:46:30 -0600

    Added reset button to reset the deck

---
+++ src/components/new-deck.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function NewDeck(props) {
+  return (
+    <>
+      <Link to='/'>Reset</Link>
+    </>
+  )
+}
